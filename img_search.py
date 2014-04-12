880e0b19a2f8c205fc9cb8ac75168f5705f3daa2
zrathustra <ahr12012@mymail.pomona.edu>
2014-04-11 21:34:08 -0700

    image search

---
+++ img_search.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import urllib2
+import json
+import random
+
+def get_image_url(q):
+
+    color = random.choice(["black", "blue", "brown", "gray", "green", "orange", "pink", "purple", "red", "teal", "white", "yellow"])
+    q.replace(' ', '%20')
+
+    url = ('https://ajax.googleapis.com/ajax/services/search/images?imgsz=small|medium|large|xlarge&safe=active&v=1.0&q='+q+"&imagecolor="+color)
+    request = urllib2.Request(url, None, {'Referer': 'testing'})
+    response = urllib2.urlopen(request)
+
+    # Get results using JSON
+    results = json.load(response)
+
+    return results['responseData']['results'][0]['unescapedUrl']
+
+if __name__=="__main__":
+    print get_image_url("government")
