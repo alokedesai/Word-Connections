fa88546d226871384a25b0b869555482d77bfd01
usuallyacorn <rllluo@gmail.com>
2014-04-12 01:42:30 -0700

    misc

--- utils.py
+++ utils.py
@@ -8,31 +8,39 @@ since2 = 454716221603475460
 
 twitter = Twython("Bj8QJnjWoPJZEm9Grk6Gw", "RWOa66BfOsE5xUXQvEJGyqE20p1hoaENbyEQypwOyMo", "33357585-OqIxndYV9Wmk0k0qCH3j32hxOZ7L2oKlAeXCeXQEz", "rwe1tmue1dzLOuCAAtbyYu1yQUfYd4lXcUMccRS4yDnpW")
 
+matrix = [[[]]]
 def score(w1, word):
-	tweets = twitter.search(q=word + ", " + w1 + "+exclude:retweets", count=100, locale="en", since_id=since2)["statuses"]
-	return len(tweets)
+	#tweets = twitter.search(q=word + ", " + w1 + "+exclude:retweets", count=100, locale="en", since_id=since2)["statuses"]
+	# return len(tweets)
+	return randint(0,5)
 		
-	
-inputs = ["house","government","family"]
 def uni(x):
         return unicodedata.normalize('NFKD',x).encode('ascii','ignore')
 
-def categoryScore(wordToCheck, category):
+def categoryScore(wordToCheck, category, index):
 	total = 0
 	size = 0
+	count = 0
+	temp = []
 	for word in category:
 		size += 1
-		total += score(word,wordToCheck)
+		sc = score(word,wordToCheck)
+		temp.append(sc)
+		total += sc
+		count += 1
+	temp.append(0)
+	matrix[index].append(temp)
 	return total/size	
 		
 # compare to category returns -1 if no category matches
 # or the index of the category the word matches
-def compareToCategory(wordToCheck, currentWords, baseLine):
+def compareToCategory(wordToCheck, currentWords, baseLine, m):
+     matrix = m
      maxScore = -1 
      maxCategory = 0 
      count = 0
      for words in currentWords:
-        currScore = categoryScore(wordToCheck, words)
+        currScore = categoryScore(wordToCheck, words, count)
         if currScore > maxScore:
           maxScore = currScore
           maxCategory = count 
