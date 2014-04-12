001c5ce58ae29d90c93843a1af4d02e5a722c212
zrathustra <ahr12012@mymail.pomona.edu>
2014-04-11 20:35:00 -0700

    derp a derp

--- index.py
+++ index.py
@@ -7,4 +7,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template("index.html")
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=3333)
