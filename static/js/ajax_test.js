50caa2eecfaf81459426761388e904b14a58aced
zrathustra <ahr12012@mymail.pomona.edu>
2014-04-11 22:20:24 -0700

    ajax test

--- static/js/ajax_test.js
+++ static/js/ajax_test.js
@@ -1,5 +1,7 @@
-function ajax(echo) {
+function testing_ajax(echo) {
     $.ajax("/echo?echo="+echo, {"dataType": "json", "type": "GET", "success": function (data) {
         console.log(data["echoed"]);
     }});
 }
+
+
